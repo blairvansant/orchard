540034aff7866751206427e7c124e72eaeb98172
blairvansant <blairbot@gmail.com>
2016-10-08 10:15:56 -0500

    es6

---
+++ javascripts/es6.js
@@ -0,0 +1,148 @@
+// console.log("hello");
+// //const*
+// const a=8;
+// console.log('a', a);
+// a=12;
+// console.log('a2', a);
+
+//const outputDiv = $('#output');
+
+
+
+
+
+
+//let*
+let b=8;
+console.log('b', b);
+b=12;
+console.log('b2', b);
+
+function varTest(){
+	var x=1;
+	if(true){
+		var x=2;
+		console.log('inside if', x);
+	}
+	console.log('outside if', x);
+}
+varTest();
+
+// function letTest(){
+// 	let y=1;
+// 	if (true){
+// 		let y=12;
+// 		console.log
+// 	}
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//arrow functions
+var sum = function(){
+	let a=1, b=2;
+	return a + b;
+}
+console.log('hardcodded sum in es5', sum());
+//
+var sum2 = () =>{
+	let a=1, b=2;
+	return a + b;
+}
+console.log('hardcodded sum in es6', sum2());
+//
+var reflect = function(value){
+	return value;
+}
+console.log("reflect in es5", reflect('es5 is so yesterday'));
+
+var reflect2 = (value) => {
+	return value;
+}
+console.log("reflect in es6", reflect2('es6is the new hotness'));
+
+var realSum = function(num1, num2){
+	return num1+num2;
+}
+console.log("realSum es5", realSum(2,3));
+
+var realSum2 = (num1, num2) =>{
+	return num1 + num2;
+}
+console.log("realSum es6", realSum2(5,4));
+
+//string templates*
+let m="biteme";
+console.log('m', m);
+//
+let r = `hello my name is ${m}`;
+console.log('r', r);
+
+//object literal property shorthand
+let wow="hi there", es6="ES6", numNum = ()=>{console.log('howdy')}
+
+let myOldObject = {
+	wow:wow,
+	es6:es6,
+	numNum:numNum
+}
+console.log('myOldObject', myOldObject);
+//
+let myNewObject = {
+	wow, es6, numNum
+}
+console.log("myNewObject", myNewObject);
+
+//property shorthand
+//method properties
+var e5Obj = {
+	foo: function(){console.log('foo')},
+	bar: function(){console.log('bar')}
+}
+console.log('e5Obj', e5Obj);
+
+var es6Obj = {
+	foo: function(){console.log('foo')},
+	bar: function(){console.log('bar')}
+}
+console.log("es6Obj", es6Obj);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
